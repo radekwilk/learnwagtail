80a32cf8d34f4a85a0e46d4260bde57a359c1fd4
Radek Wilk <radek.wilk@yahoo.co.uk>
2021-06-22 01:31:04 +0100

    creating all starting files for new Wagtail project

---
+++ rocketman/rocketman/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-da7uv@$+8!qv5l5gi4l3@q&jf-&$dihh08g*7u9n(lc3%8un)l'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
