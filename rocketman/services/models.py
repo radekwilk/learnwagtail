7324bd6f4c10f5eeeeb165c6636814ff3bb79287
Radek Wilk <radek.wilk@yahoo.co.uk>
2021-06-24 01:32:20 +0100

    add update to the ServiceListingPage class

--- rocketman/services/models.py
+++ rocketman/services/models.py
@@ -5,6 +5,8 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class ServiceListingPage(Page):
+
+    template = "services/service_listing_page.html"
     
     subtitle = models.TextField(
         blank=True,
@@ -15,6 +17,14 @@ class ServiceListingPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
     ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context['services'] = ServicePage.objects.live().public()
+
+        return context
+
+
 
 class ServicePage(Page):
 
