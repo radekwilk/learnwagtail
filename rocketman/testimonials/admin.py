d8a7765db390dbb6893040f6df2af60015342374
Radek Wilk <radek.wilk@yahoo.co.uk>
2021-06-27 02:13:50 +0100

    create testimonials app

---
+++ rocketman/testimonials/admin.py
@@ -0,0 +1,16 @@
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin, modeladmin_register
+)
+from .models import Testimonial
+
+@modeladmin_register
+class TestimonialAdmin(ModelAdmin):
+    """Testimonial admin """
+    model = Testimonial
+    menu_label = "Testimonials"
+    menu_icon = "placeholder"
+    menu_order = 290
+    add_to_settings_menu = False
+    exclude_from_explore = False
+    list_display = ("quote", "attribution")
+    search_fields = ("quote", "attribution")
