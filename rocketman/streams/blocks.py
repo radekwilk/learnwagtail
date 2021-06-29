7bf69f66b846b711399b3b4a5988874be00bc877
Radek Wilk <radek.wilk@yahoo.co.uk>
2021-06-29 03:19:55 +0100

    add new classes to create card items. Create class Cards and Link and add it to CardsBlock class

--- rocketman/streams/blocks.py
+++ rocketman/streams/blocks.py
@@ -13,31 +13,38 @@ class TitleBlock(blocks.StructBlock):
         label = "Title"
         help_text = "Centered text to display on the page"
 
+class Link(blocks.StructBlock):
+    link_text = blocks.CharBlock(
+        max_length=50,
+        default="More Details"
+    )
+    internal_page = blocks.PageChooserBlock(required=False)
+    external_link = blocks.URLBlock(required=False)
 
 
+class Card(blocks.StructBlock):
+    title = blocks.CharBlock(
+        max_length=100,
+        help_text="Bold title text for this card. Max length of 100 characters."
+    )
+    
+    text = blocks.TextBlock(
+        max_length=255,
+        help_text="Optional text for this card. Max length is 255 characters.",
+        required=False
+    )
+    
+    image = ImageChooserBlock(
+        help_text="Image will be automagically cropped to 570px by 370px"
+    )
+    link = Link(help_text="Enter a link or select a page.")
+    
+    
 class CardsBlock(blocks.StructBlock):
 
     cards = blocks.ListBlock(
-        blocks.StructBlock(
-            [
-                ("title", blocks.CharBlock(
-                    max_length=100,
-                    help_text="Bold title text for this card. Max length of 100 characters."
-                    )
-                ),
-                ("text", blocks.TextBlock(
-                    max_length=255,
-                    help_text="Optional text for this card. Max length is 255 characters.",
-                    required=False
-                    )
-                ),
-                ("image", ImageChooserBlock(
-                    help_text="Image will be automagically cropped to 570px by 370px"
-                    )
-                ),
-            ]
+          Card()
         )
-    )
 
     class Meta:
         template = "streams/cards_block.html"
