2c2406da0f6a5bd3b4f4df30f68f1346f481a31f
Radek Wilk <radek.wilk@yahoo.co.uk>
2021-07-13 03:25:47 +0100

    changes to dev.py file - doing some testing

--- rocketman/rocketman/settings/dev.py
+++ rocketman/rocketman/settings/dev.py
@@ -1,10 +1,12 @@
 from .base import *
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-da7uv@$+8!qv5l5gi4l3@q&jf-&$dihh08g*7u9n(lc3%8un)l'
+#SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
